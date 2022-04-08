fb5e66dcce2d81a89f4549d7c8f6df4dfdd38689
Gustavo Rodrigues <gsrodrigues280@gmail.com>
2022-04-08 09:55:55 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # firebase-authentication
 Firebase Backend Authentication REST API
+
+Firebase Golang ADM SDK ID Token
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"firebase-authentication/routes"
+)
+
+func main() {
+	// Setup Gin-Gonic
+	routes.GinSetup()
+}
